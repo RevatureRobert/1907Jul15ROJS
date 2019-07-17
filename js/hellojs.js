5b0e64884e914c239e3409e828463d0cfde574d0
connellrobert <robert.lel.connell@gmail.com>
2019-07-17 15:56:30 -0400

    beginning of js

---
+++ js/hellojs.js
@@ -0,0 +1,108 @@
+// console.log("hello");
+// console.warn("this is a warning");
+// console.error("this is a error")
+//console.something is going to print to the console
+
+//in js this is a single line comment
+/* in js this is a multi line comment */
+/* 
+    Javascript is NOT java
+        JavaScript has nothing to do with Java
+    A few things to know about js
+        it is a programming language
+        it is a scripting
+        it is interpreted
+            the browser will run js line by line
+        it is loosely typed
+        it is dynamic
+
+*/
+
+/* 
+    js is loosely typed
+        because variables can hold and have 
+        transferred to them any datatype
+
+    myVar=true
+    myVar="this string"
+*/
+
+/* 
+    datattypes in js
+        string, boolean, number, object, null, undefined
+*/
+
+/* 
+    how can we declare a variable?
+        var x= 454;
+        y=5;
+    as of es2015, es6
+        let z=2
+        const a=3
+
+    const does not change
+
+    Scope
+        Global      is accessible throughout the program
+        Function    is accessible throughout the function
+        Local       is accessible throughout the block of code "{}"
+*/
+//variable names
+//  cant keyword
+/*      let var = 4
+    only caracters that can begin a variable is: _,$,letters
+
+*/
+let v1=5;
+// let v;
+
+//  Operators
+//      unary, binary, ternary
+/* 
+            unary works with one operand
+    typeof returns the type of the argument
+    isNaN   returns a boolean whether the argument is a number
+    !   not operator (negation)
+    -x  flips the sign of the number
+    +x  returns the same number, or converts to a number
+    ++x pre-increment
+    x++ post-increment
+    --x pre-decrement
+    x-- post-decrement
+
+            binary works with two operands
+    +   addition
+    -   subtraction
+    *   multiplication
+    /   division
+    **  exponent
+    %   modulus
+    +=  addition assigment
+    -=  subtraction assignment
+    *=  multiplication assignment
+    /=  division assignment
+    **= exponent assignment
+    %=  modulus assignment
+and &&  logical and operator
+or  ||  logical or operator 
+xor ^   exclusive or
+    <   less than
+    >   greater than
+    <=  less than or equal to
+    >=  greater than or equal to
+    =   assignment operator
+    ==  comparison operator
+    === strict comparison
+
+
+*/
+
+/* 
+    js falsey values
+        false
+        undefined
+        null
+        0
+        ""
+        NaN
+*/
